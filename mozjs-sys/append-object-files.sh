f6cbd1226ea80aa8b50d52900bcb38b02682ad2d
Arshia Ghafoori <arshia001@live.com>
2024-04-03 13:07:01 +0330

    Fix append-object-files step

--- mozjs-sys/append-object-files.sh
+++ mozjs-sys/append-object-files.sh
@@ -3,7 +3,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 mkdir -p "${MOZ_OBJDIR}/mozjs-libs"
 
 while read -r file; do
-  cp "${MOZ_OBJDIR}/$file" "${MOZ_OBJDIR}/mozjs-libs/"
+  cp ${MOZ_OBJDIR}/$file "${MOZ_OBJDIR}/mozjs-libs/"
 done < "${script_dir}/object-files.list"
 
 llvm-ar -r ${MOZ_OBJDIR}/mozjs-libs/libjs_static_extended.a ${MOZ_OBJDIR}/mozjs-libs/*.o
