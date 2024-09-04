554c46d3f673920aca92d7fefb88699720f592bb
Arshia Ghafoori <arshia001@live.com>
2024-09-04 17:25:35 +0400

    Remove embedded gecko-dev sources

--- mozjs-sys/mozjs/js/src/jit-test/tests/basic/bug1875363.js
+++
@@ -1,9 +0,0 @@
-// |jit-test| --fuzzing-safe; --baseline-eager; --arm-hwcap=vfp; skip-if: getBuildConfiguration('pbl')
-// (skip on PBL because there is no native code on the JitScript to disassemble.)
-function f() {};
-f();
-f();
-f();
-try {
-    print(disnative(f));
-} catch (e) {}
--- mozjs-sys/mozjs/testing/web-platform/mozilla/tests/fetch/fetchpriority/support/resources/README.md
+++
@@ -1,5 +0,0 @@
-This directory contains some resources loaded by fetchpriority tests.
-
-* `generate_fonts.py` is used to generate `font_*.woff2` files.
-* `generate_media_files.py` is used to generate `audio_*.opus` and
-  `video_*.webm` files.
